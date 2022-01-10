05c48ab817b52daf0abb4a56989d64577a4a0fbe
manikdahiphale2016 <manik.dahiphale2016@gmail.com>
2022-01-10 22:21:28 +0530

    integer variable added

--- src/main/java/com/manikit/Application.java
+++ src/main/java/com/manikit/Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 	private static final String REST_URL = "https://www.equifax.com/getScores";
-
+	private Integer = 100;
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 
