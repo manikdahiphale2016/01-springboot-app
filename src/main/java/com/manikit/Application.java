5d4521a9f39b1f5dc4bc2402591b643b63f9296d
manikdahiphale2016 <manik.dahiphale2016@gmail.com>
2022-01-10 22:04:57 +0530

    HIS-301 changes

--- src/main/java/com/manikit/Application.java
+++ src/main/java/com/manikit/Application.java
@@ -1,4 +1,5 @@
 package com.manikit;
+import java.util.*;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,9 +8,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
+		private Map<String,Object> cache = new HashMap<String,Object>();
 		SpringApplication.run(Application.class, args);
 
 		int a = 10;
+	}
+	
+	//HIS-301 changes
+	public void loadDataToCache(){
+		//logic
 	}
 
 }
